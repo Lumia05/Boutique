a7456052b59fdf0f2d1d328ba4dd1e472537e24a
Lumia05 <suzielumiere07@gamil.com>
2025-09-14 06:11:12 +0100

    changements

--- database/migrations/2025_09_14_044144_create_order_items_table.php
+++ database/migrations/2025_09_14_044144_create_order_items_table.php
@@ -1,4 +1,5 @@
 <?php
+// Fichier: database/migrations/AAAA_BB_CC_create_order_items_table.php
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
@@ -8,9 +9,9 @@ return new class extends Migration
 {
     public function up(): void
     {
-        Schema::create('orders_items', function (Blueprint $table) {
+        Schema::create('order_items', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('order_id')->constrained('order')->onDelete('cascade');
+            $table->foreignId('order_id')->constrained('orders')->onDelete('cascade');
             $table->foreignId('product_id')->constrained('products');
             $table->integer('quantite');
             $table->decimal('price', 10, 2);
@@ -20,6 +21,6 @@ return new class extends Migration
 
     public function down(): void
     {
-        Schema::dropIfExists('orders_items');
+        Schema::dropIfExists('order_items');
     }
 };
